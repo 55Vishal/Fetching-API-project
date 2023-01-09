a3d2a8fa2f1843e92b2f788a32363fc0e0b8511e
55vishal <55.vishalmalusare@gmail.com>
2023-01-09 13:07:47 +0530

    Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   <section>
   <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
   <div className="container-fluid ">
-    <a className="navbar-brand" href="/#">Profile.com</a>
+    <a className="navbar-brand" to="/#">Profile.com</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -18,7 +18,7 @@ function Navbar() {
           </Link>
         </li>
         <li className="nav-item">
-        <a className="nav-link active" href="/Users">User Details
+        <a className="nav-link active" to="/Users">User Details
           <span className="visually-hidden">(current)</span>
         </a>
       </li>
